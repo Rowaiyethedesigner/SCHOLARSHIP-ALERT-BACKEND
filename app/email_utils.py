a18572342357cd6cd0689238652b192bff76e0fc
IFELOLUWA AYOMIDE ROWAIYE <Rimoraproperties@gmail.com>
2026-01-08 12:49:02 +0100

    Create email_utils.py

---
+++ app/email_utils.py
@@ -0,0 +1,10 @@
+def send_email_alert(to_email: str, subject: str, message: str):
+    """
+    Email sending stub.
+    Replace this with SendGrid / Resend later.
+    """
+    print("---- EMAIL ALERT ----")
+    print("To:", to_email)
+    print("Subject:", subject)
+    print("Message:", message)
+    print("---------------------")
